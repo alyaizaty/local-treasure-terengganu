e78626511615b4e5cc157bb9676247aa5e32320f
alyaizaty <nuraliaizzati075@gmail.com>
2026-06-25 00:00:42 +0800

    Update DBConnection for Clever Cloud

--- src/main/java/util/DBConnection.java
+++ src/main/java/util/DBConnection.java
@@ -5,9 +5,10 @@ import java.sql.DriverManager;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/ltwbs";
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String URL = "jdbc:mysql://b9xplc76zaukrdnkkz9d-mysql.services.clever-cloud.com:3306/b9xplc76zaukrdnkkz9d";
+    private static final String USER = "umuittnslrtgekow";
+    
+    private static final String PASSWORD = "JKh89OMRtWOHverymFKz"; 
 
     public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
