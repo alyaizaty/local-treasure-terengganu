91ea89c27048ff44e74fe9038402a5ab703294b3
alyaizaty <nuraliaizzati075@gmail.com>
2026-06-25 00:30:52 +0800

    Fix database connection

--- src/main/java/util/DBConnection.java
+++ src/main/java/util/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://b9xplc76zaukrdnkkz9d-mysql.services.clever-cloud.com:3306/b9xplc76zaukrdnkkz9d";
+	private static final String URL = "jdbc:mysql://b9xplc76zaukrdnkkz9d-mysql.services.clever-cloud.com:3306/b9xplc76zaukrdnkkz9d?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
     private static final String USER = "umuittnslrtgekow";
     
     private static final String PASSWORD = "JKh89OMRtWOHverymFKz"; 
