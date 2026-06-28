7bbd355759450320c8fdf8055d0ad0e93990249a
nuralia <nuralia@alya>
2026-06-28 11:48:56 +0800

    fix DB connection to Clever Cloud

--- src/main/java/util/DBConnection.java
+++ src/main/java/util/DBConnection.java
@@ -1,14 +1,11 @@
 package util;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
-
-	private static final String URL = "jdbc:mysql://b9xplc76zaukrdnkkz9d-mysql.services.clever-cloud.com:3306/b9xplc76zaukrdnkkz9d?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://b9xplc76zaukrdnkkz9d-mysql.services.clever-cloud.com:3306/b9xplc76zaukrdnkkz9d?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
     private static final String USER = "umuittnslrtgekow";
-    
-    private static final String PASSWORD = "JKh89OMRtWOHverymFKz"; 
+    private static final String PASSWORD = "JKh89OMRtWOHverymFKz";
 
     public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
